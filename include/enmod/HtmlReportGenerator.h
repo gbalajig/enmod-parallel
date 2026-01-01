83c999c7b799ea3af06329395813a3eb083c32f5
gbalajig <gurumurthy.balaji@gmail.com>
2026-01-01 15:42:03 +0530

    Add files via upload

--- include/enmod/HtmlReportGenerator.h
+++ include/enmod/HtmlReportGenerator.h
@@ -18,7 +18,9 @@ class HtmlReportGenerator {
 public:
     static void generateInitialGridReport(const Grid& grid, const std::string& path);
     static void generateSolverReport(const Solver& solver, const std::string& path);
-    static void generateSummaryReport(const std::vector<Result>& results, const std::string& path);
+    
+    // Updated: Added 'filename' parameter with a default value
+    static void generateSummaryReport(const std::vector<Result>& results, const std::string& path, const std::string& filename = "_Summary_Report.html");
 };
 
 #endif // ENMOD_HTML_REPORT_GENERATOR_H
--- include/enmod/ScenarioGenerator.h
+++ include/enmod/ScenarioGenerator.h
@@ -2,14 +2,15 @@
 #define ENMOD_SCENARIO_GENERATOR_H
 
 #include "json.hpp"
+#include "Types.h"
 #include <string>
 
 using json = nlohmann::json;
 
 class ScenarioGenerator {
 public:
-    static json generate(int size, const std::string& name);
+    // Update: Added 'obstacle_density' with a default value of 0.0
+    static json generate(int size, const std::string& name, double obstacle_density = 0.0);
 };
 
 #endif // ENMOD_SCENARIO_GENERATOR_H
-
